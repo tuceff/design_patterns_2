28a19a9978de81aa1ead32b812efd66f23fddffa
Tuğçe Şahin <f241229037@ktun.edu.tr>
2026-05-19 16:10:00 +0300

    refactor: Singleton ile RSA anahtar yönetimini ekledim.

---
+++ src/singleton/rsa_anahtar_yoneticisi.py
@@ -0,0 +1,21 @@
+from Crypto.PublicKey import RSA
+
+
+class RSAAnahtarYoneticisi:
+
+    _ornek = None
+
+    def __new__(cls):
+
+        if cls._ornek is None:
+
+            cls._ornek = super().__new__(cls)
+
+            # Anahtarlar sadece 1 kez oluşturulur
+            cls._ornek.ozel_anahtar = RSA.generate(2048)
+
+            cls._ornek.genel_anahtar = (
+                cls._ornek.ozel_anahtar.publickey()
+            )
+
+        return cls._ornek
