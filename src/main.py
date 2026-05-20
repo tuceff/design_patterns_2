344bc02ddf66bded6dd817b3aaeadf2126061bbd
Tuğçe Şahin <f241229037@ktun.edu.tr>
2026-05-20 13:15:56 +0300

    test: Sistem test edildi ve yeni main yazıldı.

--- src/main.py
+++ src/main.py
@@ -1,26 +1,21 @@
-from factory.sifreleme_factory import (
-    SifrelemeFactory
-)
+from facade.sifreleme_facade import SifrelemeFacade
 
 
-algoritma = "AES"  # "RSA" veya "BASE64" olarak değiştirilebilir
+sistem = SifrelemeFacade()
 
-sifreleyici = (
-    SifrelemeFactory
-    .sifreleyici_olustur(algoritma)
-)
+algoritma = "AES"  # "RSA" veya "BASE64"
 
 metin = "Merhaba Dünya"
 
 print("Aktif Algoritma:")
 print(algoritma)
 
-sifreli = sifreleyici.sifrele(metin)
+sifreli = sistem.sifrele(algoritma, metin)
 
 print("\nŞifreli Veri:")
 print(sifreli)
 
-cozulmus = sifreleyici.coz(sifreli)
+cozulmus = sistem.coz(algoritma, sifreli)
 
 print("\nÇözülmüş Veri:")
 print(cozulmus)
